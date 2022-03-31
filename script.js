767b7892d3d156b6b106001412b5411b0d9f3d97
DasByk <dasbyk@gmail.com>
2022-03-31 17:12:25 +0800

    31.03

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
-const date = Date();
+const date = new Date();
 
-var 
+
+function check(x){
+    if (x < 10){
+        return "0"+x;
+    }
+    else{
+        return x;
+    }
+}
+
+
+function time(){
+    const date = new Date();
+
+
+
+    var hours = check(date.getHours());
+    var minuts = check(date.getMinutes());
+    var seconds = check(date.getSeconds());
+    var mili = check(date.getMilliseconds());
+
+
+    return hours+":"+minuts+":"+seconds+":"+mili;
+
+}
+
+
+// document.getElementById("tm").innerHTML = date ;
+
+
+setInterval(() => document.getElementById("tm").innerHTML = time(), 100);
 
